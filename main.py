5f2c06e01872f3a6ca599883fa263c6b38e2c1e5
Emmanuel Kofi Dugbatey <dugbateykofiemmanuel@gmail.com>
2025-09-19 07:16:37 +0000

    Final Deploy: Set executable permissions for start.sh and add gitattributes

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 # main.py
-
 import eventlet
-# This is the crucial step: it patches the standard libraries.
+# This is the crucial step: it patches standard libraries to be asynchronous.
 eventlet.monkey_patch()
 
 # Now, import your app and socketio objects AFTER patching.
 from app import app, socketio
 
-# The main entry point for running the application.
+# This check is not strictly necessary for gunicorn but is good practice.
 if __name__ == '__main__':
     socketio.run(app, debug=True)
